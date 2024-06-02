cf6477c3cc61fe2ba577e24ee273ba05ab2a34bd
megutamago <naoya05280708@gmail.com>
2024-06-02 21:58:41 +0900

    composeファイルのcommandを各dockerfileで実行させる

---
+++ api-server/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+
+# アプリ起動
+uvicorn app.main:app --reload --host 0.0.0.0 --port 8000 &
+
+# マイグレーション実行
+alembic -c /opt/api-server/alembic.ini upgrade head
+
+# バックグラウンドで実行したuvicornプロセスを待機させる
+wait
